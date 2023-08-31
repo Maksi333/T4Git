a927a8184a46d962b7c7442df2391bba31e12407
katrine <eaa23kch@students.eaaa.dk>
2023-08-31 11:54:41 +0200

    Ny fil hihi

---
+++ src/Katrine.java
@@ -0,0 +1,13 @@
+public class Katrine {
+    public String navn;
+    public String alder;
+
+    public Katrine(String navn, String alder) {
+        this.navn = navn;
+        this.alder = alder;
+    }
+
+    public String sigHej(){
+        return "Hej!";
+    }
+}
