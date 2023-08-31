18175a2dc4a45dcd90f14d1ab7bbe115c33c9932
eaashlo <eaashlo@students.eaaa.dk>
2023-08-31 11:44:34 +0200

    Added a B

--- src/PrintStuff.java
+++ src/PrintStuff.java
@@ -3,4 +3,23 @@ public class PrintStuff {
         //This is a comment
         System.out.println("Stuff");
     }
+    public class B {
+        private boolean B;
+
+        public B(boolean b) {
+            B = b;
+        }
+
+        public void setB(boolean b) {
+            B = b;
+        }
+
+        public void isB(){
+            if (B){
+                System.out.println("A be must be, by reffrence to its beeing.");
+            } else {
+                System.out.println("A bee can not not be for then it would not be a bee.");
+            }
+        }
+    }
 }
