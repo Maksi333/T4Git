da8db3debff4db9191bcff5c817942cc596007df
Maksi333 <Simon_n_andersen@live.dk>
2023-08-31 11:44:26 +0200

    Simon

--- src/Simon.java
+++ src/Simon.java
@@ -4,5 +4,10 @@ public class Simon {
     private String lastName;
     private String profession;
 
-
+    public Simon(String name, int age, String lastName, String profession){
+        this.name = name;
+        this.age = age;
+        this.lastName = lastName;
+        this.profession = profession;
+    }
 }
